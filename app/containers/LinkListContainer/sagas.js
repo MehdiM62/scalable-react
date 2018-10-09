160dc12f87d7f1b0a26c121892faa1de0fd701e8
MehdiM62 <m_mehdi_m62@yahoo.com>
2018-10-09 15:42:40 +0800

    Routings

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -2,18 +2,18 @@
 
 import { call, put } from 'redux-saga/effects';
 import { takeLatest } from 'redux-saga';
-import { SELECT_TOPIC } from '../NavigationContainer/constants';
 import { requestLinksSucceeded, requestLinksFailed } from './actions';
+import { REQUEST_LINKS } from './constants';
 
-function fetchLinksFromServer(topic) {
-  return fetch(`http://localhost:3000/api/topics/${topic.name}/links`)
+function fetchLinksFromServer(topicName) {
+  return fetch(`http://localhost:3000/api/topics/${topicName}/links`)
     .then(response => response.json());
 }
 
 //generator function
 function* fetchLinks(action) {
   try {
-    const links = yield call (fetchLinksFromServer, action.topic);
+    const links = yield call (fetchLinksFromServer, action.topicName);
     yield put(requestLinksSucceeded(links));
   } catch (e) {
     yield put(requestLinksFailed(e.message));
@@ -23,7 +23,7 @@ function* fetchLinks(action) {
 
 // Individual exports for testing
 export function* defaultSaga() {
-  yield* takeLatest(SELECT_TOPIC, fetchLinks);
+  yield* takeLatest(REQUEST_LINKS, fetchLinks);
 }
 
 // All sagas to be loaded
